95c2f52df856f5da2e737b496aedcd303608cd77
Ranieri93 <ranieri.innocenti@gmil.com>
2019-12-17 16:42:40 +0100

    Ho quasi completato la milestone 2 e iniziato la 3

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     // definisco la variabile url:
     var apiUrlBase = 'https://api.themoviedb.org/3/';
     var apiKey = '08c1066e89bef9e87a45652373f1ec85';
+    var apiUrlImg = 'https://image.tmdb.org/t/p/'
 
     // l'evento del richiamo ajax sarà un keypress dell'inivio,
     $('#searchInput').keypress(function(event) {
@@ -92,19 +93,25 @@ $(document).ready(function(){
         var subject = array.results;
         for (var i = 0; i < subject.length; i++) {
             var titleSubject = subject[i].title;
-            var nameSubject = subject[i].name;
+
+            // if (titleSubject.length < 0) {
+            //     var titleSubject =subject[i].name;
+            // }
+
             var originalTitleSubject = subject[i].original_title;
             var originalNameSubject = subject[i].original_name;
             var languegeSubject = subject[i].original_language;
             var voteSubject = subject[i].vote_average;
+            var posterSubject = subject[i].poster_path;
             // creo il template per le variabili di handlebars:
             var templateVariables = {
-                title: titleSubject,
-                name: nameSubject,
+                posterImg: apiUrlImg + 'w342' + posterSubject,
+                title: '<strong>' + titleSubject + '</strong>',
+                name: originalNameSubject,
                 originalTitle: originalTitleSubject,
                 originalName: originalNameSubject,
-                language: languegeSubject,
-                vote: starVote(voteSubject),
+                language: flagLanguage(languegeSubject),
+                vote: starVote(voteSubject)
             }
             // infine faccio append:
             var htmlFilm = template(templateVariables);
@@ -114,9 +121,9 @@ $(document).ready(function(){
     };
 
 
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
-// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
-// lasciando le restanti vuote (troviamo le icone in FontAwesome)
+    // Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+    // permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+    // lasciando le restanti vuote (troviamo le icone in FontAwesome)
 
     function starVote (number) {
         //trasformo il numero in uno tra 1 e 5
@@ -126,11 +133,48 @@ $(document).ready(function(){
         var fullStar = '<i class="fas fa-star"></i>';
         var emptyStar = '<i class="far fa-star"></i>';
 
-        return (fullStar * rounderNumber) + [(5 - rounderNumber) * emptyStar];
-
-
-
-
+        return fullStar.repeat(rounderNumber) + emptyStar.repeat(5 - rounderNumber)
     };
+
+    // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API.
+
+    // mi creo un'altra funzione che mi permetta di gestire i casi delle lingue più conosciute:
+
+    function flagLanguage (input) {
+        //creo dei casi in cui al variare del valore dell'inpput, mi cambi anche la relativa icona:
+
+        if (input == 'en') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/united_kingdom_flags_flag_17079.png" alt="">'
+        } else if (input == 'it') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/italy_flags_flag_17018.png" alt="">'
+        } else if (input == 'de') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/germany_flags_flag_17001.png" alt="">'
+        } else if (input == 'es') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/spain_flags_flag_17068.png" alt="">'
+        } else if (input == 'fr') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/france_flags_flag_16999.png" alt="">'
+        } else if (input == 'zh') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/china_flags_flag_16985.png" alt="">'
+        } else if (input == 'pt') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/portugal_flags_flag_17054.png" alt="">'
+        } else if (input == 'ja') {
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/japan_flags_flag_17019.png" alt="">'
+        } else {
+            return input
+        };
+    };
+
+    // adesso devo gestire il l'hover sulle card con le foto:
+    $(document).on('click', '.card-film', function(){
+
+        var selectedPosterCard = $(this).children('img');
+        var selectedTextCard = $(this).children('ul');
+
+        if (!selectedPosterCard.hasClass('active')) {
+            selectedPosterCard.addClass('active')
+            selectedTextCard.addClass('active')
+        }
+
+    });
 
 });
