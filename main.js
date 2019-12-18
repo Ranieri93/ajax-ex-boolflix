dfdb5c52f9be9b7eeaf8eb75b501edfcb4164783
Ranieri93 <ranieri.innocenti@gmil.com>
2019-12-18 16:45:45 +0100

    Ho completate le 4 milestone, senza impazzire molto sul css. Ho anche provato ad adentrarmi nei bonus, ma per adesso non sono riuscito a fare molto

--- main.js
+++ main.js
@@ -39,21 +39,25 @@ $(document).ready(function(){
         if (inputValue.length != 0) {
             // svuoto prima il div e poi, tutto il resto:
             $('.container').empty();
-            imdbApiCallMovies(inputValue);
-            imdbApiCallTvSeries(inputValue);
-            // mi resetto il value per far scomparire il teso una volta cliccato invio:
+            tmdbApiCall(inputValue,'search','tv');
+            tmdbApiCall(inputValue,'search','movie');
+            // tmdbApiCall(inputValue,'find','movie');
         };
+        // mi resetto il value per far scomparire il teso una volta cliccato invio:
         var inputValue = $('#searchInput').val('');
     };
 
-    function imdbApiCallMovies (search) {
+    // tmdbApiCallGenre(query);
+
+
+    function tmdbApiCall (query, findingData, type) {
         // vado ad effettuare la chiamata ajax:
         $.ajax({
-            'url': apiUrlBase + 'search/movie',
+            'url': apiUrlBase + findingData + '/' + type ,
             'data': {
                 'api_key': apiKey,
                 // sostituisco nel query il value dell'input:
-                'query': search,
+                'query': query,
                 'language': 'it-IT'
             },
             'method': 'GET',
@@ -66,60 +70,68 @@ $(document).ready(function(){
         });
     };
 
-    function imdbApiCallTvSeries (search) {
-        // vado ad effettuare la chiamata ajax:
-        $.ajax({
-            'url': apiUrlBase + 'search/tv',
-            'data': {
-                'api_key': apiKey,
-                // sostituisco nel query il value dell'input:
-                'query': search,
-                'language': 'it-IT'
-            },
-            'method': 'GET',
-            'success': function (data) {
-                getMyInfosFromApi(data);
-            },
-            'error': function() {
-                alert('error');
-            }
-        });
-    };
-
+    // function tmdbApiCallGenre (query) {
+    //     // vado ad effettuare la chiamata ajax:
+    //     $.ajax({
+    //         'url': apiUrlBase + '/genre/tv/list',
+    //         'data': {
+    //             'api_key': apiKey,
+    //             // sostituisco nel query il value dell'input:
+    //             'query': query,
+    //             'language': 'it-IT'
+    //         },
+    //         'method': 'GET',
+    //         'success': function (data) {
+    //             getMyInfosFromApi(data);
+    //         },
+    //         'error': function() {
+    //             alert('error');
+    //         }
+    //     });
+    // };
 
     // creo una funzione che mi permetta di generalizzare la ricerca:
     function getMyInfosFromApi (array) {
         var subject = array.results;
         for (var i = 0; i < subject.length; i++) {
-            var titleSubject = subject[i].title;
-            var originalTitleSubject = subject[i].original_title;
-
-            if (titleSubject.length > 0) {
+            var subjcetI = subject[i]
+            // vado a vedere se esiste la proprietà title, se non esiste, vuol dire che è una serie, e quindi cambia la dot notation
+            if (subjcetI.hasOwnProperty('title')) {
                 var titleSubject = subject[i].title;
-            } else if (originalTitleSubject.lenght > 0) {
-                var originalTitleSubject = subject[i].original_title;
             } else {
                 var titleSubject = subject[i].name;
+            }
+
+            if (subjcetI.hasOwnProperty('original_title')) {
+                var originalTitleSubject = subject[i].original_title;
+            } else {
                 var originalTitleSubject = subject[i].original_name;
             }
 
-
-            // var originalNameSubject = subject[i].original_name;
             var languegeSubject = subject[i].original_language;
             var voteSubject = subject[i].vote_average;
-            var posterSubject = subject[i].poster_path;
+
+
+            if (subject[i].poster_path == null) {
+                console.log('trovato null');
+                var linkImg = "img/err.jpg";
+
+            } else {
+                var posterSubject = subject[i].poster_path;
+                var linkImg = apiUrlImg + 'w342' + posterSubject;
+            }
+
             var overviewSubject = subject[i].overview;
             // creo il template per le variabili di handlebars:
             var templateVariables = {
-                posterImg: apiUrlImg + 'w342' + posterSubject,
+                posterImg: linkImg,
                 title: titleSubject,
-                // name: originalNameSubject,
                 originalTitle: originalTitleSubject,
-                // originalName: originalNameSubject,
                 language: flagLanguage(languegeSubject),
                 vote: starVote(voteSubject),
                 overview: overviewSubject
             }
+
             // infine faccio append:
             var htmlFilm = template(templateVariables);
             $('.container').append(htmlFilm);
@@ -133,14 +145,34 @@ $(document).ready(function(){
 
     function starVote (number) {
         //trasformo il numero in uno tra 1 e 5
-        var rounderNumber = Math.round(number / 2);
+        var roundedNumber = Math.round(number / 2);
         // al valore del numero, corrisponderanno stelle piene: quindi icone di FA.
         // mentre quelle vuote saranno rispettivamente 5 - numero
         var fullStar = '<i class="fas fa-star"></i>';
         var emptyStar = '<i class="far fa-star"></i>';
 
-        return fullStar.repeat(rounderNumber) + emptyStar.repeat(5 - rounderNumber)
+        return fullStar.repeat(roundedNumber) + emptyStar.repeat(5 - roundedNumber);
     };
+
+    function starVoteFor (nStelline) {
+        // alternativa con un for:
+        // ne dovrò avere sempre 5, n piene e 5-n vuote.
+        // uso for per 5 star:
+        // inizializzo la stringa vuota di modo da poterci aggiungere le mie stringhe/stars:
+        var stars = '';
+        for (var i = 0; i < 5; i++) {
+            // tramite la i capisco a che stellina sono
+            if (i < nStelline) {
+                // nStelline saranno piene,
+                stars += '<i class="fas fa-star"></i>';
+            } else {
+                // nStelline vuote:
+                stars += '<i class="far fa-star"></i>';
+            }
+            // fino a che il n di stars è minore di i le mette piene, poi mi aggiungo le stelle vuote
+            return stars;
+        };
+    }
 
     // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API.
 
@@ -149,21 +181,21 @@ $(document).ready(function(){
     function flagLanguage (input) {
         //creo dei casi in cui al variare del valore dell'inpput, mi cambi anche la relativa icona:
         if (input == 'en') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/united_kingdom_flags_flag_17079.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/united_kingdom_flags_flag_17079.png" alt="ENG">'
         } else if (input == 'it') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/italy_flags_flag_17018.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/italy_flags_flag_17018.png" alt="ITA">'
         } else if (input == 'de') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/germany_flags_flag_17001.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/germany_flags_flag_17001.png" alt="DE">'
         } else if (input == 'es') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/spain_flags_flag_17068.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/spain_flags_flag_17068.png" alt="ES">'
         } else if (input == 'fr') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/france_flags_flag_16999.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/france_flags_flag_16999.png" alt="FR">'
         } else if (input == 'zh') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/china_flags_flag_16985.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/china_flags_flag_16985.png" alt="ZH">'
         } else if (input == 'pt') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/portugal_flags_flag_17054.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/portugal_flags_flag_17054.png" alt="PT">'
         } else if (input == 'ja') {
-            return '<img src="https://icon-icons.com/icons2/97/PNG/32/japan_flags_flag_17019.png" alt="">'
+            return '<img src="https://icon-icons.com/icons2/97/PNG/32/japan_flags_flag_17019.png" alt="JA">'
         } else {
             return input
         };
@@ -176,8 +208,20 @@ $(document).ready(function(){
         var selectedPosterCard = $(this).children('img');
         var selectedTextCard = $(this).children('ul');
         // pongo le conzioni
-        selectedPosterCard.toggleClass('hidden')
-        selectedTextCard.toggleClass('active')
+        selectedPosterCard.addClass('hidden')
+        selectedTextCard.addClass('active')
+    });
+
+    $(document).on('mouseleave', '.card-film', function(){
+        // mi seleziono i soggetti corretti con cui lavorare
+        var selectedPosterCard = $(this).children('img');
+        var selectedTextCard = $(this).children('ul');
+        // pongo le conzioni
+        selectedPosterCard.removeClass('hidden')
+        selectedTextCard.removeClass('active')
     });
 
 });
+
+
+// '<img src="https://image.spreadshirtmedia.net/image-server/v1/mp/products/T812A231MPA3811PT17X0Y195D138709625FS12425Cx000000/views/1,width=550,height=550,appearanceId=231,backgroundColor=F2F2F2,modelId=1111,crop=list,version=1564652308,modelImageVersion=1564481428/regalo-nerd-errore-404-img-not-found-divertente-maglietta-premium-uomo.jpg" alt="JA">'
